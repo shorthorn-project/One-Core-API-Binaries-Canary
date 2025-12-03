5cc03a6a733082a395fdae2cc9ff2ecb62db2ea1
skulltrail <samuel_4186@homail.com>
2025-12-03 11:04:04 -0300

    - Update dwrite_wrapper with new validation with steam to chromium; - Update base address of d3d11.dll for x86 and x64; - Rollback credui.dll to fix virtualbox on xp sp3; - Update UIAutomationCore.dll on x86 to prevent override version on updated xp sp3; - Fixing offline root cert update on x64 and maybe x86, what fix steam update installation (before if not update root cert steam report not connect with internet) - Rollbacking dwrgdi to try fix steam exception on xp sp3;

--- Packages/x64/Pack Installer/base/i386/wocacertinstall.bat
+++ Packages/x64/Pack Installer/base/i386/wocacertinstall.bat
@@ -1,7 +1,38 @@
-cd %~dp0
+@echo off
+setlocal enableextensions
 
-updroots authroots.sst
-updroots updroots.sst
-updroots -l roots.sst
-updroots -d delroots.sst
-updroots -u disallowedcert.sst
+REM User TEMP Folder
+set TEMPDIR=%TEMP%
+
+echo Copying files to TEMP folder %TEMPDIR%...
+copy /Y "%~dp0wauthroots.sst"        "%TEMPDIR%\authroots.sst" >nul
+copy /Y "%~dp0wupdroots.sst"         "%TEMPDIR%\updroots.sst" >nul
+copy /Y "%~dp0wroots.sst"            "%TEMPDIR%\roots.sst" >nul
+copy /Y "%~dp0wdelroots.sst"         "%TEMPDIR%\delroots.sst" >nul
+copy /Y "%~dp0wdisallowedcert.sst"   "%TEMPDIR%\disallowedcert.sst" >nul
+copy /Y "%~dp0wupdroots.exe"         "%TEMPDIR%\updroots.exe" >nul
+
+echo.
+echo going to TEMP folder...
+cd /D "%TEMPDIR%"
+
+echo.
+echo Executing updroots commands...
+updroots.exe authroots.sst
+updroots.exe updroots.sst
+updroots.exe -l roots.sst
+updroots.exe -d delroots.sst
+updroots.exe -u disallowedcert.sst
+
+echo.
+echo Cleaning temp files...
+
+del /F /Q "%TEMPDIR%\authroots.sst" >nul 2>&1
+del /F /Q "%TEMPDIR%\updroots.sst" >nul 2>&1
+del /F /Q "%TEMPDIR%\roots.sst" >nul 2>&1
+del /F /Q "%TEMPDIR%\delroots.sst" >nul 2>&1
+del /F /Q "%TEMPDIR%\disallowedcert.sst" >nul 2>&1
+del /F /Q "%TEMPDIR%\updroots.exe" >nul 2>&1
+
+echo.
+endlocal
